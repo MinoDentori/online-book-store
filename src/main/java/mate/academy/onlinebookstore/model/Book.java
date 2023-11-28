67cd6974f78752197be5283e0892fde5da06fa1d
Mykhailo Dymusha <mykhailo.dymusha@gmail.com>
2023-11-28 17:58:30 +0200

    Added book model.

---
+++ src/main/java/mate/academy/onlinebookstore/model/Book.java
@@ -0,0 +1,23 @@
+package mate.academy.onlinebookstore.model;
+
+import java.math.BigDecimal;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Entity
+@Data
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String author;
+    private String isbn;
+    private BigDecimal price;
+    private String description;
+    private String coverImage;
+}
