55af1a425f468e5c6407d987bc93fea24c104b1b
Mykhailo Dymusha <mykhailo.dymusha@gmail.com>
2023-11-28 21:47:04 +0200

    Updated Services to work with DTO classes.

--- src/main/java/mate/academy/onlinebookstore/service/BookService.java
+++ src/main/java/mate/academy/onlinebookstore/service/BookService.java
@@ -1,10 +1,14 @@
 package mate.academy.onlinebookstore.service;
 
 import java.util.List;
-import mate.academy.onlinebookstore.model.Book;
+
+import mate.academy.onlinebookstore.dto.BookDto;
+import mate.academy.onlinebookstore.dto.CreateBookRequestDto;
 
 public interface BookService {
-    Book save(Book book);
+    BookDto save(CreateBookRequestDto bookDto);
 
-    List<Book> findAll();
+    List<BookDto> findAll();
+
+    BookDto getBookById(Long id);
 }
--- src/main/java/mate/academy/onlinebookstore/service/impl/BookServiceImpl.java
+++ src/main/java/mate/academy/onlinebookstore/service/impl/BookServiceImpl.java
@@ -1,24 +1,32 @@
 package mate.academy.onlinebookstore.service.impl;
 
 import java.util.List;
-import mate.academy.onlinebookstore.model.Book;
+import lombok.RequiredArgsConstructor;
+import mate.academy.onlinebookstore.dto.BookDto;
+import mate.academy.onlinebookstore.dto.CreateBookRequestDto;
+import mate.academy.onlinebookstore.mapper.BookMapper;
 import mate.academy.onlinebookstore.repository.BookRepository;
 import mate.academy.onlinebookstore.service.BookService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+@RequiredArgsConstructor
 @Service
 public class BookServiceImpl implements BookService {
-    @Autowired
-    private BookRepository bookRepository;
+    private final BookRepository bookRepository;
+    private final BookMapper bookMapper;
 
     @Override
-    public Book save(Book book) {
-        return bookRepository.save(book);
+    public BookDto save(CreateBookRequestDto bookDto) {
+        return null;
     }
 
     @Override
-    public List<Book> findAll() {
-        return bookRepository.findAll();
+    public List<BookDto> findAll() {
+        return null;
+    }
+
+    @Override
+    public BookDto getBookById(Long id) {
+        return null;
     }
 }
