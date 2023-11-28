826694c5d0050dd30a5caa72638ecc68a94438cd
Mykhailo Dymusha <mykhailo.dymusha@gmail.com>
2023-11-28 17:59:23 +0200

    Added tests for saving and finding.

--- src/main/java/mate/academy/onlinebookstore/OnlineBookStoreApplication.java
+++ src/main/java/mate/academy/onlinebookstore/OnlineBookStoreApplication.java
@@ -1,13 +1,38 @@
 package mate.academy.onlinebookstore;
 
+import mate.academy.onlinebookstore.model.Book;
+import mate.academy.onlinebookstore.service.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.math.BigDecimal;
 
 @SpringBootApplication
 public class OnlineBookStoreApplication {
+    @Autowired
+    BookService bookService;
 
     public static void main(String[] args) {
         SpringApplication.run(OnlineBookStoreApplication.class, args);
+    }
+
+    @Bean
+    public CommandLineRunner commandLineRunner() {
+        return args -> {
+            Book book = new Book();
+            book.setAuthor("Author");
+            book.setIsbn("ISBN");
+            book.setDescription("Description");
+            book.setCoverImage("CoverImage");
+            book.setTitle("Title");
+            book.setPrice(BigDecimal.valueOf(3500.20));
+
+            bookService.save(book);
+            System.out.println(bookService.findAll());
+        };
     }
 
 }
