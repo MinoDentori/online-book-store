0a04e2859f59ea3947e48697f0226a915305a0c8
Mykhailo Dymusha <mykhailo.dymusha@gmail.com>
2023-11-28 21:46:05 +0200

    Updated Book model class. Specified table name.

--- src/main/java/mate/academy/onlinebookstore/model/Book.java
+++ src/main/java/mate/academy/onlinebookstore/model/Book.java
@@ -1,21 +1,24 @@
 package mate.academy.onlinebookstore.model;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import java.math.BigDecimal;
 import lombok.Data;
 
-@Entity
 @Data
+@Entity
+@Table(name = "book")
 public class Book {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String title;
     private String author;
+    @Column(unique = true)
     private String isbn;
     private BigDecimal price;
     private String description;
