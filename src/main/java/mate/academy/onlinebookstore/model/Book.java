3120155541f6bbdcdadcea0332b8c03b06a40a81
Mykhailo Dymusha <mykhailo.dymusha@gmail.com>
2023-12-05 15:52:58 +0200

    Added constraints to the Book entity.

--- src/main/java/mate/academy/onlinebookstore/model/Book.java
+++ src/main/java/mate/academy/onlinebookstore/model/Book.java
@@ -4,7 +4,7 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.Id;.
 import jakarta.persistence.Table;
 import java.math.BigDecimal;
 import lombok.Data;
@@ -16,10 +16,13 @@ public class Book {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(nullable = false)
     private String title;
+    @Column(nullable = false)
     private String author;
-    @Column(unique = true)
+    @Column(unique = true, nullable = false)
     private String isbn;
+    @Column(nullable = false)
     private BigDecimal price;
     private String description;
     private String coverImage;
