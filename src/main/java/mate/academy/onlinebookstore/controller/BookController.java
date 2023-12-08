61c9af36a18e410b7ddfb0de442bfa367627ca9a
Mykhailo Dymusha <mykhailo.dymusha@gmail.com>
2023-12-08 15:51:14 +0200

    Created endpoints:  1. To delete user by id.  2. To update user by id.

--- src/main/java/mate/academy/onlinebookstore/controller/BookController.java
+++ src/main/java/mate/academy/onlinebookstore/controller/BookController.java
@@ -5,11 +5,15 @@ import lombok.RequiredArgsConstructor;
 import mate.academy.onlinebookstore.dto.BookDto;
 import mate.academy.onlinebookstore.dto.CreateBookRequestDto;
 import mate.academy.onlinebookstore.service.BookService;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
@@ -31,5 +35,17 @@ public class BookController {
     @PostMapping
     private BookDto createBook(@RequestBody CreateBookRequestDto requestDto) {
         return bookService.createBook(requestDto);
+    }
+
+    @PutMapping("/{id}")
+    private BookDto updateBookById(@PathVariable Long id,
+                                   @RequestBody CreateBookRequestDto requestDto) {
+        return bookService.updateBookById(id, requestDto);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    private void deleteBookById(@PathVariable Long id) {
+        bookService.deleteBookById(id);
     }
 }
