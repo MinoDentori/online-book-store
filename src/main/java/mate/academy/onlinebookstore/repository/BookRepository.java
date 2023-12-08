ed8e6e7fcba7822c5b6241ecd26394a0512c2ec7
Mykhailo Dymusha <mykhailo.dymusha@gmail.com>
2023-12-08 22:01:17 +0200

    For now, implements JpaSpecificationExecutor<Book>

--- src/main/java/mate/academy/onlinebookstore/repository/BookRepository.java
+++
@@ -1,7 +0,0 @@
-package mate.academy.onlinebookstore.repository;
-
-import mate.academy.onlinebookstore.model.Book;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BookRepository extends JpaRepository<Book, Long> {
-}
---
+++ src/main/java/mate/academy/onlinebookstore/repository/book/BookRepository.java
@@ -0,0 +1,8 @@
+package mate.academy.onlinebookstore.repository.book;
+
+import mate.academy.onlinebookstore.model.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+
+public interface BookRepository extends JpaRepository<Book, Long>, JpaSpecificationExecutor<Book> {
+}
