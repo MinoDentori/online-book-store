1a463235d05adbdb1ba6f277f600f3423d6e0911
Mykhailo Dymusha <mykhailo.dymusha@gmail.com>
2023-12-08 22:03:22 +0200

    Added isbn specification provider.

---
+++ src/main/java/mate/academy/onlinebookstore/repository/book/spec/IsbnSpecificationProvider.java
@@ -0,0 +1,21 @@
+package mate.academy.onlinebookstore.repository.book.spec;
+
+import java.util.Arrays;
+import mate.academy.onlinebookstore.model.Book;
+import mate.academy.onlinebookstore.repository.SpecificationProvider;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.stereotype.Component;
+
+@Component
+public class IsbnSpecificationProvider implements SpecificationProvider<Book> {
+    @Override
+    public String getKey() {
+        return "isbn";
+    }
+
+    @Override
+    public Specification<Book> getSpecification(String[] params) {
+        return (root, query, criteriaBuilder) ->
+                root.get("isbn").in(Arrays.stream(params).toArray());
+    }
+}
