be3766c090b21cdad86f70f4f5270cf000e5a8ea
Mykhailo Dymusha <mykhailo.dymusha@gmail.com>
2023-12-08 22:03:27 +0200

    Added title specification provider.

---
+++ src/main/java/mate/academy/onlinebookstore/repository/book/spec/TitleSpecificationProvider.java
@@ -0,0 +1,20 @@
+package mate.academy.onlinebookstore.repository.book.spec;
+
+import java.util.Arrays;
+import mate.academy.onlinebookstore.model.Book;
+import mate.academy.onlinebookstore.repository.SpecificationProvider;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.stereotype.Component;
+
+@Component
+public class TitleSpecificationProvider implements SpecificationProvider<Book> {
+    @Override
+    public String getKey() {
+        return "title";
+    }
+
+    public Specification<Book> getSpecification(String[] params) {
+        return (root, query, criteriaBuilder) ->
+                root.get("title").in(Arrays.stream(params).toArray());
+    }
+}
