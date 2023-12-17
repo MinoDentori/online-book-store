f5f236fae713ca620cc7c0182731fd145e462bd4
Mykhailo Dymusha <mykhailo.dymusha@gmail.com>
2023-12-17 21:11:05 +0200

    Added method to handle EntityNotFoundException.

--- src/main/java/mate/academy/onlinebookstore/exception/CustomGlobalExceptionHandler.java
+++ src/main/java/mate/academy/onlinebookstore/exception/CustomGlobalExceptionHandler.java
@@ -12,6 +12,7 @@ import org.springframework.validation.FieldError;
 import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
@@ -20,6 +21,8 @@ public class CustomGlobalExceptionHandler extends ResponseEntityExceptionHandler
     private static final String TIMESTAMP = "timestamp";
     private static final String STATUS = "status";
     private static final String ERRORS = "errors";
+    private static final String MESSAGE = "message";
+    private static final String SEPARATOR = " ";
 
     @Override
     protected ResponseEntity<Object> handleMethodArgumentNotValid(
@@ -38,11 +41,21 @@ public class CustomGlobalExceptionHandler extends ResponseEntityExceptionHandler
         return new ResponseEntity<>(body, headers, status);
     }
 
+    @ExceptionHandler(EntityNotFoundException.class)
+    protected ResponseEntity<Object> handleEntityNotFound(EntityNotFoundException ex) {
+        Map<String, Object> body = new LinkedHashMap<>();
+        body.put(TIMESTAMP, LocalDateTime.now());
+        body.put(STATUS, HttpStatus.BAD_REQUEST);
+        body.put(MESSAGE, ex.getMessage());
+        return new ResponseEntity<>(body, HttpStatus.NOT_FOUND);
+    }
+
+
     private String getErrorMessage(ObjectError e) {
         if (e instanceof FieldError) {
             String field = ((FieldError) e).getField();
             String message = e.getDefaultMessage();
-            return field + " " + message;
+            return field + SEPARATOR + message;
         }
         return e.getDefaultMessage();
     }
