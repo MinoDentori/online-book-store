76a5c5c1b6343d702dab31eac463aa270015ee20
Mykhailo Dymusha <mykhailo.dymusha@gmail.com>
2023-12-19 13:50:33 +0200

    Updated register user method. For now throw exception if email already exist in DB.

--- src/main/java/mate/academy/onlinebookstore/service/user/UserService.java
+++ src/main/java/mate/academy/onlinebookstore/service/user/UserService.java
@@ -2,7 +2,8 @@ package mate.academy.onlinebookstore.service.user;
 
 import mate.academy.onlinebookstore.dto.user.UserDto;
 import mate.academy.onlinebookstore.dto.user.UserRegistrationRequestDto;
+import mate.academy.onlinebookstore.exception.RegistrationException;
 
 public interface UserService {
-    UserDto registerUser(UserRegistrationRequestDto requestDto);
+    UserDto registerUser(UserRegistrationRequestDto requestDto) throws RegistrationException;
 }
--- src/main/java/mate/academy/onlinebookstore/service/user/impl/UserServiceImpl.java
+++ src/main/java/mate/academy/onlinebookstore/service/user/impl/UserServiceImpl.java
@@ -1,8 +1,11 @@
 package mate.academy.onlinebookstore.service.user.impl;
 
+import static mate.academy.onlinebookstore.util.ConstraintsMessages.USER_WITH_THIS_EMAIL_ALREADY_EXIST;
+
 import lombok.RequiredArgsConstructor;
 import mate.academy.onlinebookstore.dto.user.UserDto;
 import mate.academy.onlinebookstore.dto.user.UserRegistrationRequestDto;
+import mate.academy.onlinebookstore.exception.RegistrationException;
 import mate.academy.onlinebookstore.mapper.UserMapper;
 import mate.academy.onlinebookstore.model.User;
 import mate.academy.onlinebookstore.repository.user.UserRepository;
@@ -14,9 +17,19 @@ import org.springframework.stereotype.Service;
 public class UserServiceImpl implements UserService {
     private final UserMapper userMapper;
     private final UserRepository userRepository;
+
     @Override
-    public UserDto registerUser(UserRegistrationRequestDto requestDto) {
-        User user = userMapper.toModel(requestDto);
+    public UserDto registerUser(UserRegistrationRequestDto requestDto)
+            throws RegistrationException {
+        if (userRepository.findByEmail(requestDto.getEmail()).isPresent()) {
+            throw new RegistrationException(USER_WITH_THIS_EMAIL_ALREADY_EXIST);
+        }
+        User user = new User();
+        user.setEmail(requestDto.getEmail());
+        user.setPassword(requestDto.getPassword());
+        user.setFirstName(requestDto.getFirstName());
+        user.setLastName(requestDto.getLastName());
+        user.setShippingAddress(requestDto.getShippingAddress());
         User savedUser = userRepository.save(user);
         return userMapper.toDto(savedUser);
     }
