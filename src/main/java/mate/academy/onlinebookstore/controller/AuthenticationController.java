eda3844b3124fe5a0e61e365b40606d6db9fa021
Mykhailo Dymusha <mykhailo.dymusha@gmail.com>
2023-12-19 13:53:19 +0200

    Added @Tag For now, throws RegistrationException.

--- src/main/java/mate/academy/onlinebookstore/controller/AuthenticationController.java
+++ src/main/java/mate/academy/onlinebookstore/controller/AuthenticationController.java
@@ -1,25 +1,31 @@
 package mate.academy.onlinebookstore.controller;
 
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import mate.academy.onlinebookstore.dto.user.UserDto;
 import mate.academy.onlinebookstore.dto.user.UserRegistrationRequestDto;
+import mate.academy.onlinebookstore.exception.RegistrationException;
 import mate.academy.onlinebookstore.service.user.UserService;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "User authentication management",
+        description = "Endpoints for managing User authentication process")
 @RequiredArgsConstructor
 @RestController
-@RequestMapping("/api/auth/registration")
+@RequestMapping("/api/auth")
 public class AuthenticationController {
-private final UserService userService;
-    @PostMapping
+    private final UserService userService;
+
+    @PostMapping("/registration")
     @Operation(summary = "Register a new user",
             description = "Register a new user using request data")
-    public UserDto registerUser(@RequestBody @Valid UserRegistrationRequestDto requestDto) {
+    public UserDto registerUser(@RequestBody @Valid UserRegistrationRequestDto requestDto)
+            throws RegistrationException {
         return userService.registerUser(requestDto);
     }
 }
