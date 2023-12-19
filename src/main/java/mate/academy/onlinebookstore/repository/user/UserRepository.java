75ba7ca5be7dbfc771126db98dcf393ea3ec5c0d
Mykhailo Dymusha <mykhailo.dymusha@gmail.com>
2023-12-19 13:48:44 +0200

    Added custom findByEmail method.

--- src/main/java/mate/academy/onlinebookstore/repository/user/UserRepository.java
+++ src/main/java/mate/academy/onlinebookstore/repository/user/UserRepository.java
@@ -1,7 +1,9 @@
 package mate.academy.onlinebookstore.repository.user;
 
+import java.util.Optional;
 import mate.academy.onlinebookstore.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
 }
