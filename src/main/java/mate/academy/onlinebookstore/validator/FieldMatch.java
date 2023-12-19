55fb5405a660ba4aab89931eea9748acb6ea0344
Mykhailo Dymusha <mykhailo.dymusha@gmail.com>
2023-12-19 13:48:26 +0200

    Created custom annotation and validator to check field matching.

--- src/main/java/mate/academy/onlinebookstore/validator/FieldMatch.java
+++ src/main/java/mate/academy/onlinebookstore/validator/FieldMatch.java
@@ -1,13 +1,13 @@
 package mate.academy.onlinebookstore.validator;
 
-import jakarta.validation.Constraint;
+import static mate.academy.onlinebookstore.util.ConstraintsMessages.VALUES_DONT_MATCH;
 
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
-
-import static mate.academy.onlinebookstore.util.ConstraintsMessages.VALUES_DONT_MATCH;
 
 @Constraint(validatedBy = FieldMatchValidator.class)
 @Target({ ElementType.TYPE })
@@ -18,4 +18,6 @@ public @interface FieldMatch {
     String field();
 
     String fieldMatch();
+    Class<?>[] groups() default {};
+    Class<? extends Payload>[] payload() default {};
 }
--- src/main/java/mate/academy/onlinebookstore/validator/FieldMatchValidator.java
+++ src/main/java/mate/academy/onlinebookstore/validator/FieldMatchValidator.java
@@ -2,9 +2,11 @@ package mate.academy.onlinebookstore.validator;
 
 import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintValidatorContext;
+import mate.academy.onlinebookstore.dto.user.UserRegistrationRequestDto;
 import org.springframework.beans.BeanWrapperImpl;
 
-public class FieldMatchValidator implements ConstraintValidator<FieldMatch, String> {
+public class FieldMatchValidator implements
+        ConstraintValidator<FieldMatch, UserRegistrationRequestDto> {
     private String field;
     private String fieldMatch;
 
@@ -12,11 +14,13 @@ public class FieldMatchValidator implements ConstraintValidator<FieldMatch, Stri
         this.field = constraintAnnotation.field();
         this.fieldMatch = constraintAnnotation.fieldMatch();
     }
+
     @Override
-    public boolean isValid(String s, ConstraintValidatorContext constraintValidatorContext) {
-        Object fieldValue = new BeanWrapperImpl(s)
+    public boolean isValid(UserRegistrationRequestDto requestDto,
+                           ConstraintValidatorContext constraintValidatorContext) {
+        Object fieldValue = new BeanWrapperImpl(requestDto)
                 .getPropertyValue(field);
-        Object fieldMatchValue = new BeanWrapperImpl(s)
+        Object fieldMatchValue = new BeanWrapperImpl(requestDto)
                 .getPropertyValue(fieldMatch);
 
         if (fieldValue != null) {
