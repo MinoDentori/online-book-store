abc6f0023a760a2f27c03d56f861f8704ac03b1f
Mykhailo Dymusha <mykhailo.dymusha@gmail.com>
2023-12-19 13:49:37 +0200

    Added new constant.

--- src/main/java/mate/academy/onlinebookstore/util/ConstraintsMessages.java
+++ src/main/java/mate/academy/onlinebookstore/util/ConstraintsMessages.java
@@ -4,6 +4,9 @@ public final class ConstraintsMessages {
     public static final String NOT_NULL_MESSAGE = "Can't be null";
     public static final String MUST_BE_POSITIVE_MESSAGE = "Must be greater than 0";
     public static final String VALUES_DONT_MATCH = "Fields values don't match!";
+    public static final String USER_WITH_THIS_EMAIL_ALREADY_EXIST =
+            "User with this email already exist";
+
     private ConstraintsMessages() {
     }
 }
