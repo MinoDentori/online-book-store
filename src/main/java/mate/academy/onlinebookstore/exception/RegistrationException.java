c6abfdedbf1bc31ce16704acc354be46f2863c1f
Mykhailo Dymusha <mykhailo.dymusha@gmail.com>
2023-12-19 10:10:47 +0200

    Created exception to throw during Authentication process.

---
+++ src/main/java/mate/academy/onlinebookstore/exception/RegistrationException.java
@@ -0,0 +1,11 @@
+package mate.academy.onlinebookstore.exception;
+
+public class RegistrationException extends Exception {
+    public RegistrationException(String message) {
+        super(message);
+    }
+
+    public RegistrationException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
