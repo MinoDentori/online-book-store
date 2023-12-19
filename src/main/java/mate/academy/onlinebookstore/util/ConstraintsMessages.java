883ba82e9cd1d2afb49a0566e5d391320b5fdcd9
Mykhailo Dymusha <mykhailo.dymusha@gmail.com>
2023-12-19 10:18:00 +0200

    Created class to store stings constants.

---
+++ src/main/java/mate/academy/onlinebookstore/util/ConstraintsMessages.java
@@ -0,0 +1,9 @@
+package mate.academy.onlinebookstore.util;
+
+public final class ConstraintsMessages {
+    public static final String NOT_NULL_MESSAGE = "Can't be null";
+    public static final String MUST_BE_POSITIVE_MESSAGE = "Must be greater than 0";
+
+    private ConstraintsMessages() {
+    }
+}
