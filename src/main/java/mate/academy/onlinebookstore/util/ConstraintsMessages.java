2f1d5fbc7c4c96e848256c308910d8304257e5e3
Mykhailo Dymusha <mykhailo.dymusha@gmail.com>
2023-12-22 08:45:02 +0200

    Added new constraints messages

--- src/main/java/mate/academy/onlinebookstore/util/ConstraintsMessages.java
+++ src/main/java/mate/academy/onlinebookstore/util/ConstraintsMessages.java
@@ -3,6 +3,7 @@ package mate.academy.onlinebookstore.util;
 public final class ConstraintsMessages {
     public static final String NOT_NULL_MESSAGE = "Can't be null";
     public static final String NOT_BLANK_MESSAGE = "Can't be blank";
+    public static final String CANT_FIND_USER_BY_EMAIL = "Can't find user by email";
     public static final String MUST_BE_POSITIVE_MESSAGE = "Must be greater than 0";
     public static final String VALUES_DONT_MATCH = "Fields values don't match!";
     public static final String USER_WITH_THIS_EMAIL_ALREADY_EXIST =
