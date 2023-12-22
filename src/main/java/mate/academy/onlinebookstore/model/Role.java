f78b5bca9dbfcf18d9a8582bb43a7a37aafaf90a
Mykhailo Dymusha <mykhailo.dymusha@gmail.com>
2023-12-22 08:46:56 +0200

    Added Role entity

---
+++ src/main/java/mate/academy/onlinebookstore/model/Role.java
@@ -0,0 +1,32 @@
+package mate.academy.onlinebookstore.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+import org.springframework.security.core.GrantedAuthority;
+
+@Entity
+@Data
+@Table(name = "role")
+public class Role implements GrantedAuthority {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private RoleName roleName;
+
+    @Override
+    public String getAuthority() {
+        return "ROLE_" + roleName.name();
+    }
+
+    public enum RoleName {
+        ADMIN,
+        USER
+    }
+}
