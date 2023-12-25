a46ccd7d516ff1160d656eefa2f9aef0a00b2f95
Mykhailo Dymusha <mykhailo.dymusha@gmail.com>
2023-12-25 21:33:25 +0200

    Fixed table name bug.

--- src/main/java/mate/academy/onlinebookstore/model/User.java
+++ src/main/java/mate/academy/onlinebookstore/model/User.java
@@ -39,7 +39,7 @@ public class User implements UserDetails {
     private String shippingAddress;
     @ManyToMany(fetch = FetchType.EAGER)
     @JoinTable(
-            name = "user_roles",
+            name = "user_role",
             joinColumns = @JoinColumn(name = "user_id"),
             inverseJoinColumns = @JoinColumn(name = "role_id")
     )
