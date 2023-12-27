892eb948bc75424db2177695d645f773a32ffa2f
Mykhailo Dymusha <mykhailo.dymusha@gmail.com>
2023-12-27 09:51:25 +0200

    Added missed @Repository annotation.

--- src/main/java/mate/academy/onlinebookstore/repository/role/RoleRepository.java
+++ src/main/java/mate/academy/onlinebookstore/repository/role/RoleRepository.java
@@ -2,6 +2,8 @@ package mate.academy.onlinebookstore.repository.role;
 
 import mate.academy.onlinebookstore.model.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface RoleRepository extends JpaRepository<Role, Long> {
 }
