4e270679b49bb7339db25cf37527ee2409ef6921
Mykhailo Dymusha <mykhailo.dymusha@gmail.com>
2023-12-28 23:24:38 +0200

    Created category entity

---
+++ src/main/java/mate/academy/onlinebookstore/model/Category.java
@@ -0,0 +1,27 @@
+package mate.academy.onlinebookstore.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
+
+@Data
+@Entity
+@Table(name = "category")
+@SQLDelete(sql = "UPDATE category SET is_deleted = true WHERE id = ?")
+@Where(clause = "is_deleted = false")
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false, unique = true)
+    private String name;
+    private String description;
+    @Column(nullable = false)
+    private boolean isDeleted = false;
+}
