7fe6e8749a82d4f4382f890a6d218128f23d359a
Mykhailo Dymusha <mykhailo.dymusha@gmail.com>
2023-12-28 23:24:00 +0200

    Added method to find all books by category id

--- src/main/java/mate/academy/onlinebookstore/repository/book/BookRepository.java
+++ src/main/java/mate/academy/onlinebookstore/repository/book/BookRepository.java
@@ -1,8 +1,15 @@
 package mate.academy.onlinebookstore.repository.book;
 
+import java.util.List;
+import mate.academy.onlinebookstore.dto.book.BookDtoWithoutCategoryIds;
 import mate.academy.onlinebookstore.model.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface BookRepository extends JpaRepository<Book, Long>, JpaSpecificationExecutor<Book> {
+    @Query("SELECT b FROM Book b JOIN FETCH b.categories c WHERE c.id = :categoryId")
+    List<BookDtoWithoutCategoryIds> findAllByCategoryId(Long categoryId);
 }
