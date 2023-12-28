94f6443918bb794131a3b3162aef17264b78d9a3
Mykhailo Dymusha <mykhailo.dymusha@gmail.com>
2023-12-28 23:23:29 +0200

    Updated mapper to set Category to Book

--- src/main/java/mate/academy/onlinebookstore/mapper/BookMapper.java
+++ src/main/java/mate/academy/onlinebookstore/mapper/BookMapper.java
@@ -1,14 +1,32 @@
 package mate.academy.onlinebookstore.mapper;
 
+import java.util.stream.Collectors;
 import mate.academy.onlinebookstore.config.MapperConfig;
 import mate.academy.onlinebookstore.dto.book.BookDto;
+import mate.academy.onlinebookstore.dto.book.BookDtoWithoutCategoryIds;
 import mate.academy.onlinebookstore.dto.book.CreateBookRequestDto;
 import mate.academy.onlinebookstore.model.Book;
+import mate.academy.onlinebookstore.model.Category;
+import org.mapstruct.AfterMapping;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
 
-@Mapper(config = MapperConfig.class)
+@Mapper(config = MapperConfig.class, uses = CategoryMapper.class)
 public interface BookMapper {
+    @Mapping(target = "categoryIds", ignore = true)
     BookDto toDto(Book book);
 
     Book toModel(CreateBookRequestDto bookDto);
+
+    BookDtoWithoutCategoryIds toBookDtoWithoutCategories(Book book);
+
+    @AfterMapping
+    default void setCategoryIds(@MappingTarget BookDto bookDto, Book book) {
+        if (!book.getCategories().isEmpty()) {
+            bookDto.setCategoryIds(book.getCategories().stream()
+                    .map(Category::getId)
+                    .collect(Collectors.toSet()));
+        }
+    }
 }
