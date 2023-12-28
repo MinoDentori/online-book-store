efe205a3570a2788fe4292395ddf21c6e23ea6ac
Mykhailo Dymusha <mykhailo.dymusha@gmail.com>
2023-12-28 23:26:46 +0200

    Created category service with implementations for endpoints methods.

---
+++ src/main/java/mate/academy/onlinebookstore/service/category/impl/CategoryServiceImpl.java
@@ -0,0 +1,59 @@
+package mate.academy.onlinebookstore.service.category.impl;
+
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import mate.academy.onlinebookstore.dto.category.CategoryDto;
+import mate.academy.onlinebookstore.dto.category.CreateCategoryRequestDto;
+import mate.academy.onlinebookstore.exception.EntityNotFoundException;
+import mate.academy.onlinebookstore.mapper.CategoryMapper;
+import mate.academy.onlinebookstore.model.Category;
+import mate.academy.onlinebookstore.repository.category.CategoryRepository;
+import mate.academy.onlinebookstore.service.category.CategoryService;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CategoryServiceImpl implements CategoryService {
+    private CategoryRepository categoryRepository;
+    private CategoryMapper categoryMapper;
+
+    @Override
+    public List<CategoryDto> findAll(Pageable pageable) {
+        return categoryRepository.findAll(pageable)
+                .stream()
+                .map(categoryMapper::toDto)
+                .toList();
+    }
+
+    @Override
+    public CategoryDto findById(Long id) {
+        Category foundCategory = categoryRepository.findById(id)
+                .orElseThrow(() ->
+                        new EntityNotFoundException("Can't get Category from DB by id:" + id));
+        return categoryMapper.toDto(foundCategory);
+    }
+
+    @Override
+    public CategoryDto save(CreateCategoryRequestDto categoryDto) {
+        Category category = categoryMapper.toEntity(categoryDto);
+        Category savedCategory = categoryRepository.save(category);
+        return categoryMapper.toDto(savedCategory);
+    }
+
+    @Override
+    public CategoryDto updateCategoryById(Long id, CreateCategoryRequestDto requestDto) {
+        Category foundCategory = categoryRepository.findById(id)
+                .orElseThrow(() ->
+                        new EntityNotFoundException("Can't get Category from DB by id:" + id));
+        Category updatedCategory = categoryMapper.toEntity(requestDto);
+        updatedCategory.setId(id);
+        categoryRepository.save(updatedCategory);
+        return categoryMapper.toDto(updatedCategory);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        categoryRepository.deleteById(id);
+    }
+}
