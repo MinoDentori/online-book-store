5b69f87f76dead8d1a8ca9b74e41a78deabea004
Mykhailo Dymusha <mykhailo.dymusha@gmail.com>
2024-01-02 23:03:22 +0200

    Added new method to CartItemRepository

--- src/main/java/mate/academy/onlinebookstore/repository/cart/CartItemRepository.java
+++ src/main/java/mate/academy/onlinebookstore/repository/cart/CartItemRepository.java
@@ -4,6 +4,9 @@ import mate.academy.onlinebookstore.model.CartItem;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface CartItemRepository extends JpaRepository<CartItem, Long> {
+    public Optional<CartItem> findByIdAndShoppingCartId(Long id, Long shoppingCartId);
 }
