1011da6f49715e6322fe48e584964002a5d8bb33
Mykhailo Dymusha <mykhailo.dymusha@gmail.com>
2024-01-02 21:23:29 +0200

    Cleaned checkstyle

--- src/main/java/mate/academy/onlinebookstore/controller/CategoryController.java
+++ src/main/java/mate/academy/onlinebookstore/controller/CategoryController.java
@@ -20,8 +20,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
 @Tag(name = "Category management", description = "Endpoints for managing Categories")
 @RestController
