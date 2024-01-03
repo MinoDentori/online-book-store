ccdb539c2c35a14858b1db162d45d3382d300b6e
Mykhailo Dymusha <mykhailo.dymusha@gmail.com>
2024-01-03 13:17:40 +0200

    Deleted redundant constraint.

--- src/main/java/mate/academy/onlinebookstore/model/ShoppingCart.java
+++ src/main/java/mate/academy/onlinebookstore/model/ShoppingCart.java
@@ -13,12 +13,12 @@ import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.ToString;
 import org.hibernate.annotations.SQLDelete;
-import org.hibernate.annotations.Where;
+import org.hibernate.annotations.SQLRestriction;
 
 @Data
 @Entity
 @SQLDelete(sql = "UPDATE shopping_cart SET is_deleted = true WHERE id = ?")
-@Where(clause = "is_deleted = false")
+@SQLRestriction("is_deleted = false")
 @Table(name = "shopping_cart")
 public class ShoppingCart {
     @Id
