1a445132e295228734fb0fbbc68cd30c69195971
Mykhailo Dymusha <mykhailo.dymusha@gmail.com>
2024-01-08 23:18:56 +0200

    Created SQL scripts for DBc

---
+++ src/test/resources/database/book/insert-into-book.sql
@@ -0,0 +1,1 @@
+INSERT INTO book (id, title, author, isbn, price, description) VALUES (1, 'The Buried Giant', 'Kazuo Ishiguro', '80-902734-1-6', 434.00, 'The Buried Giant is a fantasy novel')
---
+++ src/test/resources/database/category/insert-into-category.sql
@@ -0,0 +1,1 @@
+INSERT INTO category (id, name, description) VALUES (1, 'Fantasy', 'Fantasy category')
