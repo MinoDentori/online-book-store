36c526427d90209e481b915c22ba99484e985f24
Mykhailo Dymusha <mykhailo.dymusha@gmail.com>
2024-01-10 11:53:20 +0200

    Changed data

--- src/test/resources/database/book/insert-into-book.sql
+++ src/test/resources/database/book/insert-into-book.sql
@@ -1,1 +1,1 @@
-INSERT INTO book (id, title, author, isbn, price, description) VALUES (1, 'The Buried Giant', 'Kazuo Ishiguro', '80-902734-1-6', 434.00, 'The Buried Giant is a fantasy novel')
+INSERT INTO book (id, title, author, isbn, price, description) VALUES (1, 'The Buried Giant', 'Kazuo Ishiguro', '9781408711705', 434.00, 'The Buried Giant is a fantasy novel')
